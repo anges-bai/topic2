278406a4fba0254d680fa3791fa39e6eb23fe783
anges-bai <45257069+anges-bai@users.noreply.github.com>
2020-04-03 18:31:30 +0800

    Create scanner.java

---
+++ scanner.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+/**
+ * @Author: BaiMia
+ * @Description:
+ */
+public class test8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            long num = 0;
+            for (int i = 2; i < line.length(); i++) {
+                num *= 16;
+                char ch = line.charAt(i);
+                int n = 0;
+                if (ch >= 'A' && ch <= 'F') {
+                    n = ch - 'A' + 10;
+                } else if (ch >= 'a' && ch <= 'f') {
+                    n = ch - 'a' + 10;
+                } else {
+                    n = ch - '0';
+                }
+                num += n;
+            }
+            System.out.println(num);
+        }
+    }
+}
+
